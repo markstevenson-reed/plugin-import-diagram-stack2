9b7f7ee478e2cf76be133d91666245f7b91bec67
daniel-lewis-reed <daniel.lewis@reed.com>
2024-07-03 11:59:05 +0100

    gdfgdf

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,30 @@
+import { CommonModule } from '@angular/common'
 import { Component, signal, WritableSignal } from '@angular/core'
+import { FormsModule } from '@angular/forms'
 import { bootstrapApplication } from '@angular/platform-browser'
 import 'zone.js'
 
 @Component({
   selector: 'app-root',
   standalone: true,
+  imports: [
+    CommonModule,
+    FormsModule
+  ],
   templateUrl: './main.html',
   styleUrl: './main.scss'
 })
 export class App {
+  rawText: WritableSignal<string> = signal('')
   diagram: WritableSignal<string> = signal('[Customer]<->[Address]->[office]->[test]')
+
+  clear() {
+    alert('clear')
+  }
+
+  submit() {
+    alert('submit')
+  }
 }
 
 bootstrapApplication(App)
