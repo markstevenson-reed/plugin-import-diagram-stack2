7c143b40aae1745ff191805ebe60d3342cc3a101
daniel-lewis-reed <daniel.lewis@reed.com>
2024-07-03 11:14:42 +0100

    dsadsa

--- src/main.ts
+++ src/main.ts
@@ -5,14 +5,7 @@ import 'zone.js';
 @Component({
   selector: 'app-root',
   standalone: true,
-  template: `
-    <h1>Hello world test from {{ name }}!</h1>
-    <textarea></textarea>
-    <a target="_blank" href="https://angular.dev/overview">
-      Learn more about Angular
-    </a>
-    <img src="http://yuml.me/diagram/scruffy/class/{{ diagram }}" >
-  `,
+  templateUrl: './main.html',
 })
 export class App {
   name = 'Angular';
