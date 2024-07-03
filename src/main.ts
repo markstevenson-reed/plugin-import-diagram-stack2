5fbdfab7a54635f6dffe09b7a18820978cf69dd7
daniel-lewis-reed <daniel.lewis@reed.com>
2024-07-03 12:38:16 +0100

    gfdgdf

--- src/main.ts
+++ src/main.ts
@@ -15,16 +15,36 @@ import 'zone.js'
   styleUrl: './main.scss'
 })
 export class App {
-  rawText: WritableSignal<string> = signal('')
+  mainRawText: WritableSignal<string> = signal('')
+  testRawText: WritableSignal<string> = signal(`[XmsWebapp]<-[XmsOrders]
+  [XmsOrders]<-[XmsFulfilment]
+  [XmsOrders]<-[XmsConfig]
+  [XmsCore]<-[XmsRoles]
+  [XmsLookup]<-[XmsCommon]
+  [XmsRoles]<-[XmsLookup]
+  [XmsRoles]<-[XmsSecurity]
+  [XmsFulfilment]<-[XmsCore]
+  [XmsConfig]<-[XmsCommonConfig]
+  [XmsConfig]<-[XmsCore]`)
   diagram: WritableSignal<string> = signal('[Customer]<->[Address]->[office]->[test]')
   errors: WritableSignal<string[]> = signal([])
 
-  clear() {
-    this.rawText.set('')
+  clearMain() {
+    this.mainRawText.set('')
   }
 
-  submit() {
+  submitMain() {
     this.process()
+  }
+
+  clearTest() {
+    this.mainRawText.set('')
+  }
+
+  submitTest() {
+    let txt: string = this.testRawText()
+    txt = txt.replaceAll('\n',', ')
+    this.diagram.set(txt)
   }
 
   private process() {
