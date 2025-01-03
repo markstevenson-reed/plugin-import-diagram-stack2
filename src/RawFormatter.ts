4e8b10c43527c5f718aec8ebd8323ca47efaba11
markstevenson-reed <mark.stevenson@reed.com>
2025-01-03 12:07:35 +0000

    packing code into boxes

---
+++ src/RawFormatter.ts
@@ -0,0 +1,56 @@
+export class RawFormatter {
+
+  private static ARROW: string = '<-'
+  private static PROJECT_NAME: string = 'xms-platform-g3'
+
+  private static isFirstRowToInspect(row: string): boolean {
+    return !row.includes('(excluded)') && row.includes(this.PROJECT_NAME)
+  }
+
+  private static replaceMultipleSpaces(input: string): string {
+    return input.replace(/\s{2,}/g, ' ');
+  }
+
+  private static cleanMe(input: string, lastPlugin: string): string {
+    //input = this.replaceMultipleSpaces(input)
+    let cleaned = input
+    .replace(/        '+this.PROJECT_NAME'+'\..*/gi, '') //Remove any lines like xms-platform-g3.XmsSomething  (4 usages found) 
+    .replace(/                    \d{2,3} compile project/gi, '') //89 compile project
+    .replace(/                build.gradle  .*/gi, '')
+    .replace(/  \(\d{1,2} usage.* found\)/gi, ']')
+    .replace(/            /gi, '[')
+    .replace(/\(':/gi, lastPlugin + this.ARROW + '[') //Add the arrow
+    .replace(/'\)/gi, ']')
+    console.log(input, cleaned)
+    return cleaned;
+  }
+
+  private static rowHasArrow(input: string): boolean {
+    return input.includes(this.ARROW)
+  }
+
+  static format(txtArr: string[]): string[]{
+    let umlArr: string[] = []
+    let atFirstRowToInspect: boolean = false
+    let lastPlugin: string  = ''
+
+    txtArr.forEach((row) => {
+      atFirstRowToInspect = atFirstRowToInspect ? true : this.isFirstRowToInspect(row)
+      
+      //if line has exclusionLine start from here    
+      if(atFirstRowToInspect){
+        row = this.cleanMe(row,lastPlugin)
+        if(row){
+          //IF row does not have arrow THEN set last plugin name
+          if(!this.rowHasArrow(row)){
+            lastPlugin = row
+          } else {
+            umlArr.push(row)
+          }
+        }
+      }
+    })
+    return umlArr
+  }
+
+}
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { Component, signal, WritableSignal } from '@angular/core'
 import { FormsModule } from '@angular/forms'
 import { bootstrapApplication } from '@angular/platform-browser'
 import 'zone.js'
+import { isDependancyRedundant } from './UmlFormatter'
+import { umlTextDefault } from './UmlFormatter'
+import { createDependenciesByPlugin } from './UmlFormatter'
+import { RawFormatter } from './RawFormatter'
 
 // Starting example of raw data
 const rawTextDefault: string = `
@@ -30,19 +34,6 @@ Found Occurrences  (268 usages found)
                     94 compile project(':XmsSecurity')
                     95 compile project(':XmsLookup')
   `
-  // Starting example of uml text data
-  const umlTextDefault: string = `[XmsWebapp]<-[XmsOrders]
-  [XmsOrders]<-[XmsFulfilment]
-  [XmsOrders]<-[XmsConfig]
-  [XmsCore]<-[XmsRoles]
-  [XmsLookup]<-[XmsCommon]
-  [XmsRoles]<-[XmsLookup]
-  [XmsRoles]<-[XmsSecurity]
-  [XmsFulfilment]<-[XmsCore]
-  [XmsConfig]<-[XmsCommonConfig]
-  [XmsConfig]<-[XmsRoles]
-  [XmsConfig]<-[XmsCore]`
-
   const javaScriptTextDefault: string = ``
 
   // Diagram default design
@@ -60,8 +51,6 @@ Found Occurrences  (268 usages found)
 })
 
 export class App {
-  projectName: string = 'xms-platform-g3'
-  exclusionLine: string = 'Occurrence in Gradle build script'
   rawText: WritableSignal<string> = signal(rawTextDefault)
   umlText: WritableSignal<string> = signal(umlTextDefault)
   typeScriptText: WritableSignal<string> = signal('')
@@ -88,13 +77,13 @@ export class App {
 
   //TODO rename
   formatTypeScriptToUml() {
-    const dependenciesByPlugin = this.createDependenciesByPlugin( this.umlText() )
+    const dependenciesByPlugin = createDependenciesByPlugin( this.umlText() )
     let umlCleanArr: string[] = []
 
     dependenciesByPlugin.forEach((dependencies,plugin) => {
       dependencies.forEach((dependency)=>{
-        if(!this.isDependancyRedundant(dependenciesByPlugin, plugin, dependency)){
-          umlCleanArr.push( `[${plugin}] <- [${dependency}]`)
+        if(!isDependancyRedundant(dependenciesByPlugin, plugin, dependency)){
+          umlCleanArr.push( `[${plugin}]<-[${dependency}]`)
         }
       })
     })
@@ -104,89 +93,19 @@ export class App {
   }
 
   formatUmlToTypeScript() {
-    const dependenciesByPlugin = this.createDependenciesByPlugin( this.umlText() )
+    const dependenciesByPlugin = createDependenciesByPlugin( this.umlText() )
     let umlCleanArr: string[] = []
 
     dependenciesByPlugin.forEach((dependencies,plugin) => {
       umlCleanArr.push( `'${plugin}': {`)
 
       dependencies.forEach((dependency)=>{
-        umlCleanArr.push(`\t'${dependency}', ${this.isDependancyRedundant(dependenciesByPlugin, plugin, dependency) ? '// Remove this':''}`)
+        umlCleanArr.push(`\t'${dependency}', ${isDependancyRedundant(dependenciesByPlugin, plugin, dependency) ? '// Remove this':''}`)
       })
       umlCleanArr.push( `},`)
       
     })
     this.typeScriptText.set(umlCleanArr.join('\n'))
-  }
-
-  // Function to check if a dependency is redundant
-  private isDependancyRedundant(dependencies: Map<string, Set<string>>, start: string, target: string): boolean {
-    const visited = new Set<string>();
-
-    //Depth-First Search
-    function dfs(node: string): boolean {
-        if (node === target) return true;
-        if (visited.has(node)) return false;
-        visited.add(node);
-
-        const neighbors = dependencies.get(node);
-        if (neighbors) {
-            for (const neighbor of neighbors) {
-                if (dfs(neighbor)) return true;
-            }
-        }
-        return false;
-    }
-
-    const neighbors = dependencies.get(start);
-    if (neighbors) {
-        for (const neighbor of neighbors) {
-            if (neighbor !== target && dfs(neighbor)) {
-                return true;
-            }
-        }
-    }
-    return false;
-  }
-
-  // {'XmsWebapp' => Set(1), 'XmsOrders' => Set(2), 'XmsFulfilment' => Set(1), 'XmsConfig' => Set(3), 'XmsCore' => Set(1), …}
-  private createDependenciesByPlugin(umlText: string): Map<string, Set<string>>{
-    const umlArr: string[] = umlText.split('\n')
-    const dependenciesByPlugin = new Map<string, Set<string>>()
-
-    function addPluginAndDependancy(row: string){
-      // [XmsWebapp]<-[XmsOrders] => ['XmsWebapp', 'XmsOrders']
-      function getPluginAndDependancy(row: string): string[] {
-        // Regular expression to match content inside square brackets
-        const regex = /\[([^\]]+)\]/g;
-        const plugins = [];
-        let match;
-        while ((match = regex.exec(row)) !== null) {
-          plugins.push(match[1]);
-        }
-        return plugins
-      }
-
-      function getDependencies(plugin: string){
-        let dependencies = dependenciesByPlugin.get(plugin)
-        // Make plugin ref if not found
-        if(!dependencies){
-          dependencies = new Set<string>()
-          dependenciesByPlugin.set(plugin, dependencies)
-        }
-        return dependencies
-      }
-
-      const pluginAndDependancy: string[] = getPluginAndDependancy(row)
-      let dependencies = getDependencies(pluginAndDependancy[0])
-      dependencies.add(pluginAndDependancy[1])
-      getDependencies(pluginAndDependancy[1])
-    }
-
-    umlArr.forEach((row) => {
-      addPluginAndDependancy(row)
-    })
-    return dependenciesByPlugin
   }
 
   submitUml() {
@@ -202,56 +121,21 @@ export class App {
     this.errors.set([])
   }
 
-  private cleanMe(input: string, lastPlugin: string): string {
-    input = input.replace(/        xms-platform-g3\..*/gi, '')
-    input = input.replace(/                    \d{2,3} compile project/gi, '')
-    input = input.replace(/                build.gradle  .*/gi, '')
-    input = input.replace(/  \(\d{1,2} usage.* found\)/gi, ']')
-    input = input.replace(/            /gi, '[')
-    input = input.replace(/\(':/gi, lastPlugin+'<-[') //Add the arrow
-    input = input.replace(/'\)/gi, ']')
-    return input
-  }
-
-  private rowHasArrow(input: string): boolean {
-    return input.includes('<-')
-  }
-
   //Format the raw contents into UML
   private process() {
     //test
     let txt: string = this.rawText()
-    let exclusionLine: string = this.exclusionLine
     let txtArr: string[] = txt.split('\n')
-    let found: boolean = false
-    let umlArr: string[] = []
-    let lastPlugin: string  = ''
+    let umlArr: string[] = RawFormatter.format(txtArr)
 
     this.errors.set([])
-
-    txtArr.forEach((row) => {
-    
-      //if line has exclusionLine start from here    
-      if(found){
-        row = this.cleanMe(row,lastPlugin)
-        if(row){
-          //IF row does not have arrow THEN set last plugin name
-          if(!this.rowHasArrow(row)){
-            lastPlugin = row
-          } else {
-            umlArr.push(row)
-          }
-        }
-      }
-      //Check for exlcusion line
-      found = found ? true : row.includes(exclusionLine)
-    })
 
     // do stuff
     // e.g. update txt with regex
     // e.g. set errors
-    if(!found){
-      this.errors.set(['unable to find removal line'])
+    if(!umlArr.length){
+      this.errors.set(['UML lines are empty. Likely unable to find first line'])
+      console.log(txtArr)
     }
 
     if (!this.errors().length) {
